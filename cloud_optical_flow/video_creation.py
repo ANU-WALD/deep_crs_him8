762f00989cc8cbfc94eef7072c1ab975fbba1a85
prl900 <pablo.larraondo@anu.edu.au>
2019-03-30 22:58:42 +1100

    Simple CNN added and optical flow video creator

--- cloud_optical_flow/video_creation.py
+++ cloud_optical_flow/video_creation.py
@@ -8,10 +8,13 @@ fig = plt.figure()
 ds = xr.open_dataset("/data/sat_precip/H8_Flow.nc")
 print(ds)
 print(ds.time)
+#print(ds["B7"][:,:,:].min())
+#print(ds["B7"][:,:,:].max())
+#sys.exit()
 
 ims = []
 for i in range(350):
-    im = plt.imshow(ds["B7"][i,:,:], animated=True)
+    im = plt.imshow(ds["B7"][i,:,:], animated=True, vmin=200, vmax=400)
     ims.append([im])
 
 ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
--- cnn/model.py
+++ cnn/model.py
@@ -1,62 +1,72 @@
-import tensorflow.keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization, Conv2D, Conv2DTranspose, Dropout
 from tensorflow.keras.optimizers import Adam
+from matplotlib import pyplot as plt
 import numpy as np
-import sys
 
 def GetModel():
     model = Sequential()
     model.add(BatchNormalization(axis=3, input_shape=(400, 400, 3)))
-    model.add(Conv2D(16, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(32, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(64, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(32, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(16, (1, 1), activation='relu', padding='same'))
-    model.add(Conv2D(1, (1, 1), activation='relu', padding='same'))
+    # Size 400x400x3
+    model.add(Conv2D(32, (5, 5), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 200x200x32
+    model.add(Conv2D(64, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 100x100x64
+    model.add(Conv2D(128, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 50x50x128
+    model.add(Conv2D(256, (5, 5), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 25x25x256
+    model.add(Conv2DTranspose(128, (5, 5), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 50x50x128
+    model.add(Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 100x100x64
+    model.add(Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    # Size 200x200x32
+    model.add(Conv2DTranspose(1, (5, 5), strides=(2, 2), activation='relu', padding='same'))
+    # Size 400x400x1
 
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001), metrics=['mae'])
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001), metrics=['mae'])
 
     print(model.summary())
 
     return model
 
-b7 = np.load("/scratch/director2107/CRS_Data/b7.npy")
-b7 = (b7 - b7.min()) / (b7.max() - b7.min())
-"""
-b8 = np.load("/scratch/director2107/CRS_Data/b8.npy")
-b8 = (b8 - b8.min()) / (b8.max() - b8.min())
-b9 = np.load("/scratch/director2107/CRS_Data/b9.npy")
-b9 = (b9 - b9.min()) / (b9.max() - b9.min())
-b10 = np.load("/scratch/director2107/CRS_Data/b10.npy")
-b10 = (b10 - b10.min()) / (b10.max() - b10.min())
-b11 = np.load("/scratch/director2107/CRS_Data/b11.npy")
-b11 = (b11 - b11.min()) / (b11.max() - b11.min())
-"""
-b12 = np.load("/scratch/director2107/CRS_Data/b12.npy")
-b12 = (b12 - b12.min()) / (b12.max() - b12.min())
-"""
-b13 = np.load("/scratch/director2107/CRS_Data/b13.npy")
-b13 = (b13 - b13.min()) / (b13.max() - b13.min())
-"""
-b14 = np.load("/scratch/director2107/CRS_Data/b14.npy")
-b14 = (b14 - b14.min()) / (b14.max() - b14.min())
-"""
-b15 = np.load("/scratch/director2107/CRS_Data/b15.npy")
-b15 = (b15 - b15.min()) / (b15.max() - b15.min())
-b16 = np.load("/scratch/director2107/CRS_Data/b16.npy")
-b16 = (b16 - b16.min()) / (b16.max() - b16.min())
-"""
+# Load and normalise satellite reflectances (Try just 3 bands [8,10,14]
+b8 = np.load("/data/sat_precip/b8_30.npy")
+b8 = (b8 - b8.mean()) / b8.std()
+b10 = np.load("/data/sat_precip/b10_30.npy")
+b10 = (b10 - b10.mean()) / b10.std()
+b14 = np.load("/data/sat_precip/b14_30.npy")
+b14 = (b14 - b14.mean()) / b14.std()
 
-x = np.stack((b7, b12, b14), axis=3)[:200,:]
-#x = np.stack((b7, b8, b9, b10, b11, b12, b13, b14, b15, b16), axis=3)
-y = np.expand_dims(np.load("/scratch/director2107/CRS_Data/crs.npy"), axis=3)[:200,:]
-#y = (y - y.min()) / (y.max() - y.min())
+# Stack reflectances in depth to create X (input of the network)
+x = np.stack((b8, b10, b14), axis=3)
 
-print("in mean", x.mean())
-print("out mean", y.mean())
-print(x.shape)
-print(y.shape)
+# Load measured precipitation to create Y (output of the network)
+y = np.load("/data/sat_precip/gpm_30.npy")[:,:,:,None]
 
+# Verify dimensions of the data. 
+# We should have 1163 samples of 400x400 images for both X and Y
+print(x.shape, y.shape)
+
+# Instantiate model defined in function above
 model = GetModel()
-history = model.fit(x, y, epochs=300, verbose=1, validation_split=.2, shuffle=True)
+
+# Fit data using a 70/30 validation split
+history = model.fit(x, y, epochs=10, verbose=1, validation_split=.3, shuffle=True)
+
+# Save the model once trained for later use
+#model.save('cnn_rain.h5')
+
+# Generate some data to see if model makes sense
+y_pred = model.predict(x[:1, :, :, :])
+print(y_pred)
+plt.imsave("pred_rain.png", y_pred[0,:,:,0])
+plt.imsave("obs_rain.png", y[0,:,:,0])
